2606db5d19362ec925dc5db20f204515845b6986
matthewbodie <bodiemd@gmail.com>
2025-03-02 21:48:05 -0500

    adding home page content

---
+++ src/data.ts
@@ -0,0 +1,8 @@
+export const myName = "Matthew Bodie";
+export const introduction = 
+  "Pleased to meet you! I'm currently the Executive Director of " +
+  "Learning Resources at St. Petersburg College, and a seasoned " +
+  "college administrator with 13 years of leadership experience in " +
+  "building and enhancing programs, partnerships, and initiatives " +
+  "centered on student and faculty success.";
+
